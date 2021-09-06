0b67c803288a3c082825f6a6ffe5dc89cbc0c69a
fsdexter <fsdexter@gmail.com>
2021-09-06 02:59:31 +0000

    test

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Dropdown from "./dropdown";
 import { Context } from "../store/appContext";
 import logo from "../../img/logo.png";
 
@@ -12,8 +11,48 @@ export const Navbar = () => {
 			<Link to="/">
 				<img src={logo} style={{ width: "8rem" }} />
 			</Link>
-			<div>
-				<Dropdown title="Favorites" items={store.fave} />
+			<div className="dropdown myDropdown">
+				<button
+					className="btn btn-outline-warning dropdown-toggle pl-5 pr-5"
+					type="button"
+					id="dropdownMenuButton"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false">
+					Favourites
+				</button>
+				<div className="dropdown-menu myFavourites" aria-labelledby="dropdownMenuButton">
+					<ul className="text-warning">
+						{store.fave && store.fave.length > 0 ? (
+							store.fave.map(item => {
+								return (
+									<li
+										key={item.url}
+										className="dropdown-item text-warning d-flex justify-content-between align-items-center"
+										id="myLiList">
+										<h6>{item.name}</h6>
+										<Link to="/details">
+											<button
+												type="button"
+												className="btn btn-primary"
+												onClick={() => {
+													actions.showDetails(item.url);
+												}}>
+												More
+											</button>
+										</Link>
+										<i
+											className="fa fa-heart text-danger"
+											onClick={() => actions.changeHeart(item.id, false, item.type)}
+										/>
+									</li>
+								);
+							})
+						) : (
+							<p className="pl-3">You do not have any favourite</p>
+						)}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
